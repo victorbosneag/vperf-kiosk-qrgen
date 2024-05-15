ddb4bb8f3019e142488192f4b5c342071c8fbf60
victorbosneag <victor.bosneag@hotmail.com>
2024-05-16 01:49:38 +0300

    added design files to qr code generation

--- card_creator.py
+++ card_creator.py
@@ -7,33 +7,36 @@ import io
 
 FONT = "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf"
 
-def create_card(input_text, input_id):
-	qrcode = segno.make("{" + '"name":"' + input_text +'","id":' + str(input_id) + "}", error='h')
-	out = io.BytesIO()
-	qrcode.save(out, scale=5, kind='png', dark="#475574")
-	out.seek(0)
-	img = Image.open(out).convert('RGB')
+def create_card(input_text, input_id, design_file):
+    qrcode = segno.make("{" + '"name":"' + input_text +'","id":' + str(input_id) + "}", error='h')
+    out = io.BytesIO()
+    qrcode.save(out, scale=5, kind='png', dark="#475574")
+    out.seek(0)
+    img = Image.open(out).convert('RGB')
 
-	logo = Image.open("logo2.png").convert('RGBA')
-	logo_aspect_ratio = logo.width / logo.height
-	logo = logo.resize((int(logo_aspect_ratio * (img.height//4)),img.height//4),Image.LANCZOS)
+    logo = Image.open("logo3.png").convert('RGBA')
+    logo_aspect_ratio = logo.width / logo.height
+    logo = logo.resize((int(logo_aspect_ratio * (img.height//4)),img.height//4),Image.LANCZOS)
+
+    design = Image.open(design_file).convert('RGBA')
+    design_aspect_ratio = design.width / design.height
+    design = design.resize((int(design_aspect_ratio * (design.height//2)),design.height//2),Image.LANCZOS)
+
+    bg_img = Image.new('RGB', (img.width + logo.width + 250, 245), "white")
+    bg_img.paste(img, (0,0))
+    bg_img.paste(logo,(img.width+10, 20), mask=logo)
+    bg_img.paste(design,(img.width+logo.width+10+10, 20), mask=design)
+    draw = ImageDraw.Draw(bg_img)
+    font = ImageFont.truetype(FONT, 25, encoding='unic')
+    id_font = ImageFont.truetype(FONT, 15, encoding='unic')
+
+    offset = img.height//4 + img.width//2
+
+    for line in wrap(input_text, width=35):
+        draw.text((img.width+10+8, offset), line, font=font, fill="#475574")
+        offset += font.getsize(line)[1]
+
+    draw.text((bg_img.width-25, bg_img.height-20), str(input_id), font=id_font, fill="#000000")
 
 
-	bg_img = Image.new('RGB', (img.width + logo.width + 15, 245), "white")
-	bg_img.paste(img, (0,0))
-	bg_img.paste(logo,(img.width+10, 20), mask=logo)
-
-	draw = ImageDraw.Draw(bg_img)
-	font = ImageFont.truetype(FONT, 25, encoding='unic')
-	id_font = ImageFont.truetype(FONT, 15, encoding='unic')
-
-	offset = 20 + img.height//4 + 10
-
-	for line in wrap(input_text, width=35):
-	    draw.text((img.width+10+8, offset), line, font=font, fill="#475574")
-	    offset += font.getsize(line)[1]
-
-	draw.text((bg_img.width-25, bg_img.height-20), str(input_id), font=id_font, fill="#000000")
-
-
-	bg_img.save(f"qrcodes/{input_id} - {input_text}.png")
+    bg_img.save(f"qrcodes/{input_id} - {input_text} - {design_file}.png")
